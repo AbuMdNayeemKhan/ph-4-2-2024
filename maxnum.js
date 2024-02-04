966713ac358486521737d1b1231483d371ed9d4b
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-04 21:00:07 +0600

    man number define

---
+++ maxnum.js
@@ -0,0 +1,14 @@
+let num1 = 88;
+let num2 = 98;
+let num3 = 79;
+// print the bigger number for thos value by using if else
+
+if(num1 > num2 && num1 > num3){
+    console.log('The bigger number is: ' + num1);
+} else if (num2 > num1 && num2 > num3){
+    console.log('The bigger number is: ' + num2);
+} else {
+    console.log('The bigger number is: ' + num3);
+}
+
+// print the bigger number for those value by using ja max function
