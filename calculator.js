933b065a3eee8e74ac2c435dc0ab3286b4183491
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-05 23:27:35 +0600

    a calculator create by using function

---
+++ calculator.js
@@ -0,0 +1,29 @@
+//calculator
+function add(num1, num2){
+    return num1 + num2;
+}
+function sub(num1, num2){
+    return num1 - num2;
+}
+function mul(num1, num2){
+    return num1 * num2;
+}
+function div(num1, num2){
+    return num1 / num2;
+}
+
+function calculator(num1, num2, cal){
+    if (cal === '+') {
+         console.log(add(num1, num2));
+    } else if (cal === '-'){
+        console.log(sub(num1, num2));
+    } else if (cal === '-'){
+        console.log(mul(num1, num2));
+    } else if (cal === '*'){
+        console.log(div(num1, num2));
+    } else {
+        console("No aratematick oparator found.");
+    };
+};
+
+calculator(2, 2, '+');
