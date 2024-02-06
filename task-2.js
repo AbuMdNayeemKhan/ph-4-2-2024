5527d12a210d2e4c12749d46f98e6e7aeb7f68a5
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-06 20:13:30 +0600

    task 2 done

--- task-2.js
+++ task-2.js
@@ -1,2 +1,13 @@
 // Find the friend with the smallest name.
-const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+let names = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+function chotoNam(para){
+    let lowName = para[0];
+    for (let item of para) {
+        if(item.length < lowName.length){
+            lowName = item;
+        }
+    }
+    return lowName;
+}
+let print = chotoNam(names);
+console.log(print);
